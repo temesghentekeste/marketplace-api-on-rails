873072a0e49036d25aae22195a21a2ec9f6c5a77
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-01 14:23:54 +0300

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    
     validates :email, uniqueness: true
     validates_format_of :email, with: /@/
     validates :password_digest, presence: true
