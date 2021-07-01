4368b2b527917bb930ee00b555322be19d2eb156
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-01 15:10:33 +0300

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-
+    before_action :set_user, only: %i[show update]
      # GET /users/1
      def show 
         render json: User.find(params[:id]) 
@@ -16,9 +16,22 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
+    # PATCH/PUT /users/1
+    def update
+        if @user.update(user_params)
+            render json: @user, status: :ok
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
     private
     # Only allow a trusted parameter "white list" through.
     def user_params
         params.require(:user).permit(:email, :password)
     end 
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -14,17 +14,26 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
 
     test "should create user" do
       assert_difference('User.count') do
-        post api_v1_users_url,params:{user:{email:
-          'test@test.org', password: '123456' } },as: :json
+        post api_v1_users_url,params:{user:{email:'test@test.org', password: '123456' } },as: :json
       end
       assert_response :created
     end
 
     test "should not create user with taken email" do
       assert_no_difference('User.count') do
-        post api_v1_users_url, params: { user: { email: @user
-        .email, password: '123456' } }, as: :json
+        post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
       end
+      assert_response :unprocessable_entity
+    end 
+
+    test "should update user" do
+      patch api_v1_user_url(@user), params: { user: { email:@user.email, password: '123456' } }, as: :json
+      assert_response :success
+    end
+    
+    test "should not update user when invalid params are sent" do
+      patch api_v1_user_url(@user), params: { user: { email:
+      'bad_email', password: '123456' } }, as: :json
       assert_response :unprocessable_entity
     end 
 end
