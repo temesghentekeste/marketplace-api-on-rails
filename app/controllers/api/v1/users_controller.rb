645611af69418abf99e3438205147dd177f23a07
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-01 20:43:51 +0300

    Add ruby-jwt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
 
     before_action :set_user, only: %i[show update destroy]
      # GET /users/1
-     def show 
+    def show 
         render json: @user
     end
 
