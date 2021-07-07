8ba554da2eb06a861fd2799a7cfeb108902de1eb
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-07 11:05:18 +0300

    Adds search scopes on the product model

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -90,4 +90,29 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
            as: :json
     assert_response :forbidden
   end
+
+  test "should filter products by name" do
+    assert_equal 2, Product.filter_by_title('tv').count
+  end
+
+  test 'should filter products by name and sort them' do
+    assert_equal [products(:another_tv), products(:one)],
+                                         Product.filter_by_title('tv').sort
+  end 
+
+  test 'should filter products by price and sort them' do
+    assert_equal [products(:two), products(:one)], 
+                                  Product.above_or_equal_to_price(200).sort
+  end 
+
+  test 'should filter products by price lower and sort them' do
+    assert_equal [products(:two), products(:one)], 
+                                  Product.lower_or_equal_to_price(200).sort
+  end 
+
+  test 'should sort product by most recent' do
+    # we will touch some products to update them
+    products(:two).touch
+    assert_equal [products(:another_tv), products(:one),products(:two)], Product.recent.to_a
+  end
 end
