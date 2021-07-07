99a65de3526db1d45271e8591fde60b56aa11f18
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-07 20:43:28 +0300

    Adds quantity to placements

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,13 +11,15 @@ class Order < ApplicationRecord
     self.total = products.map(&:price).sum
   end
 
-  # @param product_ids_and_quantities [Array<Hash>] something like this `[{product_id: 1, quantity: 2}]`
+ # @param product_ids_and_quantities [Array<Hash>] something like this `[{product_id: 1, quantity: 2}]`
   # @yield [Placement] placements build
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
-      product_ids_and_quantities.each do |product_id_and_quantity|
-        placement = placements.build(product_id: product_id_and_quantity[:product_id])
-
-        yield placement if block_given?
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      placement = placements.build(
+        product_id: product_id_and_quantity[:product_id],
+        quantity: product_id_and_quantity[:quantity],
+      )
+      yield placement if block_given?
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_171440) do
+ActiveRecord::Schema.define(version: 2021_07_07_173732) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_07_07_171440) do
     t.integer "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity", default: 0
     t.index ["order_id"], name: "index_placements_on_order_id"
     t.index ["product_id"], name: "index_placements_on_product_id"
   end
