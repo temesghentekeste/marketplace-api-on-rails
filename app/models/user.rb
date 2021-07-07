ba2c2217cc64b6b3252b964d6e455284bcc72240
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-07 12:21:44 +0300

    Generate orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :products, dependent: :destroy
+    has_many :orders, dependent: :destroy
 
     validates :email, uniqueness: true
     validates_format_of :email, with: /@/
