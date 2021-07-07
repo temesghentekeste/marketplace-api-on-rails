1ecdbbfe875be3e81a437b7aea2c34816501ea6c
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-08 00:40:07 +0300

    Adds pagination for orders index action

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -34,15 +34,16 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should show orders' do
-    get api_v1_orders_url,
-    headers: { Authorization: JsonWebToken.encode(user_id:  @order.user_id) }, as: :json
-   
-    assert_response :success 
-    
-    json_response = JSON.parse(response.body)
+    get api_v1_orders_url, headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) }, as: :json
+    assert_response :success
 
-    assert_equal @order.user.orders.count, json_response['data'].count
-  end  
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    assert_equal @order.user.orders.count, json_response[:data].count
+    assert_not_nil json_response.dig(:links, :first)
+    assert_not_nil json_response.dig(:links, :last)
+    assert_not_nil json_response.dig(:links, :prev)
+    assert_not_nil json_response.dig(:links, :next)
+  end 
   
   test 'should show order' do
     get api_v1_order_url(@order),
